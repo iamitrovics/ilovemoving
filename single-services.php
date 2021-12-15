46f0fdedd3bce534154348c2bafdd22b598adaaa
Isidora Kalicanin <isidorakalicanin@madeonline.agency>
2021-12-15 13:56:45 +0100

    services update

--- single-services.php
+++ single-services.php
@@ -31,9 +31,10 @@ $container = get_theme_mod( 'understrap_container_type' );
     </header>
     <!-- /#inner-header -->
 
-    <section id="regular-page">
+    <section id="blog-page">
         <div class="container">
             <div class="row">
+                <div class="col-md-12">
                 <?php
                 // check if the flexible content field has rows of data
                 if( have_rows('flexible_content_services') ):?>
@@ -51,9 +52,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 					                $column = get_sub_field('content_column');
 					                ?>  
 
-                                    <div class="col-lg-12">
-                                 <?php echo $column; ?>
-                             </div>
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <?php echo $column; ?>
+                                        </div>
+                                    </div>
 
 				                <?php endwhile;
 
@@ -61,30 +64,127 @@ $container = get_theme_mod( 'understrap_container_type' );
 				                echo 'No data';
 			                endif;
 			                ?>
-      
-                <?php elseif( get_row_layout() == '2_column' ):?>
-                    <?php
+                        
+                        <?php elseif( get_row_layout() == 'accordion' ): ?>
 
-			                // check if the repeater field has rows of data
-			                if(have_rows('content_block_2')):
+                            <div class="content__accordion">
+                                <h2><?php the_sub_field('accordion_title'); ?></h2>
+                                <?php
+                                    // check if the repeater field has rows of data
+                                    if(have_rows('accordion_list')):
+                                        // loop through the rows of data
+                                        while(have_rows('accordion_list')) : the_row();
+                                            $title = get_sub_field('heading');
+                                            $content  = get_sub_field('content');
+                                            ?>  
+                                            <div class="faq-wrap">
+                                                <h3 class="accordion-heading"><?php echo $title; ?></h3>
+                                                <div class="content">
+                                                    <?php echo $content; ?>
+                                                </div>
+                                            </div>
+                                        <?php endwhile;
+                                    else :
+                                        echo 'No data';
+                                    endif;
+                                    ?>
+                            </div>
+                            <!-- /#faq__accordion -->
 
-				                // loop through the rows of data
-				                while(have_rows('content_block_2')) : the_row();
+                        <?php elseif( get_row_layout() == 'full_width_content' ): ?>
 
-					                $column = get_sub_field('content_column');
+                            <div class="full-content">
+                                <?php the_sub_field('content_block'); ?>
+                            </div>
 
-					                ?>  
+                        <?php elseif( get_row_layout() == 'full_width_image' ): ?>
 
-                                    <div class="col-lg-6">
-                                 <?php echo $column; ?>
-                             </div>
+                            <div class="featured-photo">
+                                <?php
+                                $imageID = get_sub_field('featured_image');
+                                $image = wp_get_attachment_image_src( $imageID, 'side-image' );
+                                $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                ?> 
 
-				                <?php endwhile;
+                                <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" /> 
+                                <div class="caption">
+                                    <?php the_sub_field('image_caption'); ?>
+                                </div>
+                            </div>
 
-			                else :
+                        <?php elseif( get_row_layout() == 'image_left_text_right' ): ?>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="featured-photo">
+                                        <?php
+                                        $imageID = get_sub_field('featured_image');
+                                        $image = wp_get_attachment_image_src( $imageID, 'side-image' );
+                                        $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                        ?> 
+
+                                        <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" /> 
+                                        <div class="caption">
+                                            <?php the_sub_field('image_caption'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="full-content">
+                                        <?php the_sub_field('content_block'); ?>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        <?php elseif( get_row_layout() == 'image_right_text_left' ): ?>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="full-content">
+                                        <?php the_sub_field('content_block'); ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="featured-photo">
+                                        <?php
+                                        $imageID = get_sub_field('featured_image');
+                                        $image = wp_get_attachment_image_src( $imageID, 'side-image' );
+                                        $alt_text = get_post_meta($imageID , '_wp_attachment_image_alt', true);
+                                        ?> 
+
+                                        <img class="img-responsive" alt="<?php echo $alt_text; ?>" src="<?php echo $image[0]; ?>" /> 
+                                        <div class="caption">
+                                            <?php the_sub_field('image_caption'); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <?php elseif( get_row_layout() == '2_column' ):?>
+                            <div class="row">
+                            <?php
+
+                                // check if the repeater field has rows of data
+                                if(have_rows('content_block_2')):
+
+                                    // loop through the rows of data
+                                    while(have_rows('content_block_2')) : the_row();
+
+                                        $column = get_sub_field('content_column');
+
+                                        ?>  
+                        
+                                        <div class="col-lg-6">
+                                            <?php echo $column; ?>
+                                        </div>
+
+                                    <?php endwhile;
+
+                                else :
 				                echo 'No data';
 			                endif;
 			                ?>
+                            </div>
 
                     <?php endif;?>
                 <?php endwhile;?>
@@ -97,6 +197,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
                     <?php get_template_part( 'content-form-services' ); ?>
 
+                </div>
             </div>
             <!-- /.row -->
         </div>
